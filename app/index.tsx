7909872f46702506e2bfb2dbd1f79b4c61409d9c
Karl George <karlgeorge87@gmail.com>
2024-05-13 14:25:44 +0200

    Add breaking news and padding

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,18 @@
+import BreakingNews from '@/components/BreakingNews';
 import SearchInput from '@/components/SearchInput';
 import { ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
   return (
-    <SafeAreaView className='h-full bg-primary'>
+    <SafeAreaView className='h-full px-4 bg-primary'>
       <ScrollView>
         <View className='items-center justify-center'>
           <SearchInput />
-          <Text>Hello World!</Text>
+        </View>
+        <View>
+          <Text className='text-xl text-text_primary'>Breaking News</Text>
+          <BreakingNews />
         </View>
       </ScrollView>
     </SafeAreaView>
