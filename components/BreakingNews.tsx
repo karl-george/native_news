4d6b61e1fab51390f2a60c45e0f5f874647f3d73
Karl George <karlgeorge87@gmail.com>
2024-05-13 14:25:52 +0200

    Add breaking news

---
+++ components/BreakingNews.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import NewsCardLarge from './NewsCardLarge';
+
+const BreakingNews = () => {
+  return (
+    <View>
+      <NewsCardLarge />
+    </View>
+  );
+};
+
+export default BreakingNews;
