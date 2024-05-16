5d15a586633b35cbcc4bbe71f10b0e7ed14280b5
Karl George <karlgeorge87@gmail.com>
2024-05-16 12:13:02 +0200

    Style news cards

--- components/NewsCardLarge.tsx
+++ components/NewsCardLarge.tsx
@@ -1,19 +1,31 @@
+import { NewsArticle } from '@/typings';
 import { View, Text, Image } from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
 // TODO: Fix this type
-const NewsCardLarge = (news: any) => {
+const NewsCardLarge = ({ newsArticle }: NewsArticle) => {
+  const { title, description, image_url, source_id } = newsArticle;
+
   return (
-    <View className='mt-2 rounded-md bg-card'>
-      <Image className='w-[175px] h-[150px]' />
-      <View>
-        <Text className='text-text_primary'>Headline</Text>
-        <Text className='text-sm text-text_secondary'>Subtitles</Text>
-        <View>
-          <Text className='text-xs text-text_primary'>News Center</Text>
-          <Text className='text-xs text-text_primary'>Date</Text>
+    <Animatable.View className='mt-2 mr-5 rounded-md bg-card w-[250px] h-full'>
+      <View className='gap-2 p-3'>
+        <Image
+          src={image_url}
+          className='w-[225px] h-[150px]'
+          resizeMode='cover'
+        />
+        <Text className='text-text_primary'>{title.substring(0, 60)}...</Text>
+        <Text className='text-sm text-text_secondary'>
+          {description.substring(0, 80)}...
+        </Text>
+        <View className='flex-row justify-between'>
+          <Text className='text-[10px] text-text_primary'>
+            {source_id.toUpperCase()}
+          </Text>
+          <Text className='text-[10px] text-text_primary'>Date</Text>
         </View>
       </View>
-    </View>
+    </Animatable.View>
   );
 };
 
