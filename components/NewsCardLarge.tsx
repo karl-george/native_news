145edfa18376abafa537b7b0bcf3735a56f22636
Karl George <karlgeorge87@gmail.com>
2024-05-16 13:35:27 +0200

    Update news card styling

--- components/NewsCardLarge.tsx
+++ components/NewsCardLarge.tsx
@@ -8,24 +8,24 @@ const NewsCardLarge = ({ newsArticle }: NewsArticle) => {
   const { title, description, image_url, source_id, pubDate } = newsArticle;
 
   return (
-    <Animatable.View className='mt-2 mr-5 rounded-md bg-card w-[250px] h-full'>
-      <View className='gap-2 p-3'>
-        <Image
-          src={image_url}
-          className='w-[225px] h-[150px]'
-          resizeMode='cover'
-        />
-        <Text className='text-text_primary'>{title.substring(0, 60)}...</Text>
-        <Text className='text-sm text-text_secondary'>
-          {description.substring(0, 80)}...
-        </Text>
-        <View className='flex-row justify-between'>
-          <Text className='text-[10px] text-text_primary'>
-            {source_id.toUpperCase()}
+    <Animatable.View className='mr-3'>
+      <View className='w-[250px] h-[332px] p-3 rounded-md bg-card space-y-3'>
+        <View>
+          <Image
+            src={image_url}
+            className='w-full h-40 rounded-md'
+            resizeMode='cover'
+          />
+        </View>
+        <View className='h-[140px] justify-between'>
+          <Text className='text-text_primary'>{title.substring(0, 40)}...</Text>
+          <Text className='text-text_secondary'>
+            {description.substring(0, 90)}...
           </Text>
-          <Text className='text-[10px] text-text_primary'>
-            {postedAt(pubDate)}
-          </Text>
+          <View className='flex-row justify-between'>
+            <Text className='text-text_primary'>{source_id}</Text>
+            <Text className='text-text_primary'>{postedAt(pubDate)}</Text>
+          </View>
         </View>
       </View>
     </Animatable.View>
