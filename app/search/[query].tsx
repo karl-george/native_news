d46c4552f8cdc0999e27d8d2ab4b5567b709badd
Karl George <karlgeorge87@gmail.com>
2024-05-17 14:32:25 +0200

    Style search page

--- app/search/[query].tsx
+++ app/search/[query].tsx
@@ -1,26 +1,46 @@
-import { View, Text, FlatList } from 'react-native';
-import React, { useState } from 'react';
-import { useLocalSearchParams } from 'expo-router';
+import NewsCardSmall from '@/components/NewsCardSmall';
+import SearchInput from '@/components/SearchInput';
+import { Feather } from '@expo/vector-icons';
+import { router, useLocalSearchParams } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { FlatList, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Search = () => {
-  const query = useLocalSearchParams();
+  const { query } = useLocalSearchParams();
+  const [news, setNews] = useState([]);
 
-  // const fetchData = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&q=pizza`
-  //     );
-  //     const res = await response.json();
-  //     setNews(res);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&q=${query}&language=en`
+      );
+      const res = await response.json();
+      setNews(res.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [query]);
 
   return (
-    <SafeAreaView className='h-full bg-primary'>
-      {/* <FlatList /> */}
+    <SafeAreaView className='h-full px-4 bg-primary'>
+      <View className='flex-row items-center justify-between'>
+        <TouchableOpacity
+          className='p-2 rounded-full bg-card'
+          onPress={() => router.back()}
+        >
+          <Feather name='arrow-left' size={24} color='#6A717A' />
+        </TouchableOpacity>
+        <SearchInput />
+      </View>
+      <FlatList
+        data={news}
+        renderItem={({ item }) => <NewsCardSmall newsArticle={item} />}
+      />
     </SafeAreaView>
   );
 };
